a25f18a69b541d39c92dd5c074a215b9568568f4
AlexandrTarasov <usproinfo@gmail.com>
2017-11-30 22:31:30 +0200

    Create typing_tutor.js

---
+++ typing_tutor.js
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+<style type="text/css">
+	.main{text-align: center; width: 100%; border: 1px solid red; margin-top:200px;}
+	.left{width:25%; margin:5px 0 5px 0; padding:5px; font-size: 30px; border: 1px solid blue; text-align: right;  display: inline-block;
+				border-right-color:#eaeaea; color:#c0c0c0;}
+	.right{width:25%; margin:5px 0 5px 0; padding:5px; font-size: 30px; border: 1px solid green; text-align: left; display: inline-block;
+				border-left-color:#eaeaea;}	
+</style>
+
+<script>
+var left_div  = [];
+var right_div = ["s", "4", "z", " ", "1", "h", "l"];
+
+
+</script>
+
+</head>
+<body style="margin:auto 0; text-align: center;">
+	<div class="main">
+		<div class="left">3</div><div class="right">4345</div>
+	</div>
+</body>
+</html>
+
