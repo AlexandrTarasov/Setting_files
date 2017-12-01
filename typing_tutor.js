3caaee7753fabe34e46a2bddcb0518fd7690acff
AlexandrTarasov <usproinfo@gmail.com>
2017-12-01 12:07:08 +0200

    Update typing_tutor.js

--- typing_tutor.js
+++ typing_tutor.js
@@ -3,6 +3,7 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Document</title>
+	<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
 <style type="text/css">
 	.main{text-align: center; width: 100%; border: 1px solid red; margin-top:200px;}
 	.left{width:25%;  margin:5px 0 5px 0; padding:5px; font-size: 30px; border: 1px solid blue; text-align: right;  display: inline-block;
@@ -12,36 +13,51 @@
 </style>
 
 </head>
-<body style="margin:auto 0; text-align: center;">
-	<div class="main">
-		<div class="left" id="l">3</div><div class="right" id="r" onclick="onkeypress()"></div>
+<body style="margin:auto 0; text-align: center;" class="display">
+	<div class="main" aria-live="polite" aria-atomic="true">
+		<div class="left" id="l"> </div><div class="right" id="r" contenteditable=true onkeypress="Type()"></div>
+		
+		<p class="text-display"></p>
 	</div>
-
+</body>
 <script>
+
+
 var left_div  = [];
 var right_div = ["B", "s", " ", "l"];
 let buffer = '';
+let char = '';
+
+
+$( ".right" ).keypress(function() {
+  		char = event.key;	
+});
+
 
 document.getElementById("r").innerHTML = right_div.join('');
 
+	function Type() {
 
-function Type() {
+			
+			//char = document.getElementById("r").innerHTML = String.fromCharCode(event.keyCode);
+			//char = document.querySelector('.text-display').innerHTML = keyCodes[e.keyCode];
 
-		if(right_div.length>0){
-    	buffer = right_div.shift();
-    	left_div.push(buffer); 
-    	document.getElementById("r").innerHTML = right_div;
-    	document.getElementById("l").innerHTML = left_div.join('');
-  	}
-  	else{alert("Усё напечатал")}
+			if(right_div.length>0){
+	    	buffer = right_div.shift();
+	    	left_div.push(buffer); 
+	    	document.getElementById("r").innerHTML = right_div;
+	    	document.getElementById("l").innerHTML = left_div.join('');
+	  	}
+	  	else{alert("Усё напечатал");}
 
-}
+	  	console.log(char+" and "+buffer)
+
+	};
+
+
+
+
 
 </script>
 
-</body>
-
 </html>
-
-
-
