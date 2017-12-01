2c94b86dbd2ba5944a107590c7c29f1008dbed7a
AlexandrTarasov <usproinfo@gmail.com>
2017-12-01 12:29:53 +0200

    Update typing_tutor.js

--- typing_tutor.js
+++ typing_tutor.js
@@ -15,7 +15,7 @@
 </head>
 <body style="margin:auto 0; text-align: center;" class="display">
 	<div class="main" aria-live="polite" aria-atomic="true">
-		<div class="left" id="l"> </div><div class="right" id="r" contenteditable=true onkeypress="Type()"></div>
+		<div class="left" id="l"> </div><div class="right" id="r" contenteditable=true ></div>
 		
 		<p class="text-display"></p>
 	</div>
@@ -29,35 +29,37 @@ let buffer = '';
 let char = '';
 
 
-$( ".right" ).keypress(function() {
-  		char = event.key;	
-});
-
 
 document.getElementById("r").innerHTML = right_div.join('');
 
-	function Type() {
+$( ".right" ).keypress(function() {
+	
+	event.preventDefault(); //prevent defult 
+  char = event.key;	
 
-			
-			//char = document.getElementById("r").innerHTML = String.fromCharCode(event.keyCode);
-			//char = document.querySelector('.text-display').innerHTML = keyCodes[e.keyCode];
+  document.getElementById("r").innerHTML = right_div.join('');
+  
+  if(char == right_div[0]){
+	  if(right_div.length>0){
+		  buffer = right_div.shift();
+		  left_div.push(buffer); 
+		  document.getElementById("r").innerHTML = right_div;
+		  document.getElementById("l").innerHTML = left_div.join('');
+		}
+		else{alert("Усё напечатал");}
+	}
+	else{ alert("Ошибка"); }
+		
+	console.log(char+" and "+buffer);	
+	
 
-			if(right_div.length>0){
-	    	buffer = right_div.shift();
-	    	left_div.push(buffer); 
-	    	document.getElementById("r").innerHTML = right_div;
-	    	document.getElementById("l").innerHTML = left_div.join('');
-	  	}
-	  	else{alert("Усё напечатал");}
 
-	  	console.log(char+" and "+buffer)
-
-	};
-
-
-
+});
 
 
 </script>
 
 </html>
+
+
+
