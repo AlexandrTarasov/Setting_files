1e37e54d6af8760fbfc1258367a9f1a766b645b2
AlexandrTarasov <usproinfo@gmail.com>
2017-12-01 14:46:07 +0200

    Update typing_tutor.js

--- typing_tutor.js
+++ typing_tutor.js
@@ -5,32 +5,35 @@
 	<title>Document</title>
 	<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
 <style type="text/css">
-	.main{text-align: center; width: 100%; border: 1px solid red; margin-top:200px;}
-	.left{width:25%;  margin:5px 0 5px 0; padding:5px; font-size: 30px; border: 1px solid blue; text-align: right;  display: inline-block;
-				border-right-color:#eaeaea; color:#c0c0c0;}
-	.right{width:25%;  margin:5px 0 5px 0; padding:5px; font-size: 30px; border: 1px solid green; text-align: left; display: inline-block;
-				border-left-color:#eaeaea;}	
+	.body{width: 100%;
+    height: 100%; position: absolute;
+    top: 0;
+    left: 0;
+    overflow: auto; }
+
+	.main{text-align: center; width: 725px;  overflow: auto; margin: 100px auto; display: table;}
+	.left{width:350px; height:35px;    display: inline-block;margin:auto; padding:5px; font-size: 30px; border: 1px solid blue; text-align: right; overflow: hidden;
+				border-right-color:#eaeaea; color:#c0c0c0; }
+
+	.right{width:350px; height:35px; margin:auto;  padding:5px; font-size: 30px; border: 1px solid green; text-align: left;  display: inline-block;
+				border-left-color:#eaeaea; float: right;}	
 </style>
 
 </head>
-<body style="margin:auto 0; text-align: center;" class="display">
-	<div class="main" aria-live="polite" aria-atomic="true">
-		<div class="left" id="l"> </div><div class="right" id="r" contenteditable=true ></div>
-		
-		<p class="text-display"></p>
-	</div>
+<body class="body">
+	<div class="main" aria-live="polite" aria-atomic="true"><div class="left" id="l">&#9998;</div><input class="right" id="r" value="" ></div>
 </body>
 <script>
 
-
-var left_div  = [];
-var right_div = ["Im", "want", "lear", "english", 'and', 'now', 'it', 'advansed'];
+let right_str = '';
+var left_div  = '';
+var right_input = ["I want learn English to advanced level"];
 let buffer = '';
 let char = '';
+let typo_amout=0;
 
 
-
-document.getElementById("r").innerHTML = right_div.join(' ');
+right_str = document.getElementById("r").value  = right_input[0]
 
 $( ".right" ).keypress(function() {
 	
@@ -39,16 +42,18 @@ $( ".right" ).keypress(function() {
 
   
   
-	if(right_div.length>0){
-  	if(char == right_div[0]){
-		  buffer = right_div.shift();
-		  left_div.push(buffer); 
-		  document.getElementById("r").innerHTML = right_div.join(' ');
-		  document.getElementById("l").innerHTML = left_div.join(' ');
+	if(right_str.length>0){
+  	if(char == right_str.charAt(0)){
+		  
+		  buffer = right_str.charAt(0);
+		  right_str = right_str.slice(1);
+		  left_div= left_div+buffer; 
+		  document.getElementById("r").value = right_str;
+		  document.getElementById("l").innerHTML = left_div;
 		}
-		else{ alert("Ошибка"); }
+		else{ alert("Typo"); typo_amout++;}
 	}
-	else{alert("Усё напечатал");}
+	else{alert("Усё напечатал. Опечаток "+typo_amout);}
 		
 	console.log(char+" and "+buffer);	
 	
